cc02941cd0dd594180522e0b9a73635b22fcc4aa
Raul Flores <rmfcortes@msn.com>
2019-01-15 22:50:50 -0600

    Listo para produccion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interfaces';
-import { resolve } from 'path';
-import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
